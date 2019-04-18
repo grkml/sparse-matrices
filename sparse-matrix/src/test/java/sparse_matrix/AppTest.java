5fc75275959e9389e039f8aaf331dbefbc0ffd9c
Gurkamal Singh <gurkamalpsc@gmail.com>
2019-04-17 21:41:26 -0700

    Added in a modified ArrayList class and a modified LinkedList class for this project into a package called abstract_data_type

--- sparse-matrix/src/test/java/sparse_matrix/AppTest.java
+++ sparse-matrix/src/test/java/sparse_matrix/AppTest.java
@@ -4,11 +4,9 @@
 package sparse_matrix;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+public class AppTest
+{
 }
